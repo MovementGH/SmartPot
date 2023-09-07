3285ffe9168235c842bf6be94836ecf32c92da7e
Movement <48330137+MovementGH@users.noreply.github.com>
2023-09-07 15:59:35 -0400

    Basic API Connection

--- Cloud/Client/src/App.js
+++ Cloud/Client/src/App.js
@@ -1,11 +1,41 @@
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <p>Hello There!</p>
-    </div>
-  );
+//Libs
+import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+
+//Contexts
+import { UserContext } from './Contexts/User';
+import Request from './Tools/Request';
+
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state={Context:1};
+  }
+
+  componentDidMount() {
+    Request.CacheableRequest('GET','user').then(Result=>{
+      this.setState({Context:{User:Result.Body}});
+    })
+  }
+
+  render() {
+    if(this.state.Context) {
+      return (
+        <UserContext.Provider value={this.state.Context}>
+          <div className="d-flex flex-column" style={{overflow:"hidden",minHeight:"100vh"}}>
+            <Routes>
+
+              <Route path="/" element = {<p>Main Page</p>}/>
+
+            </Routes>
+          </div>
+        </UserContext.Provider>
+      );
+    }
+    return null;
+  }
 }
 
 export default App;
--- Cloud/Server/index.js
+++ Cloud/Server/index.js
@@ -1,15 +1,70 @@
 const LibExpress = require('express');
+const LibCors=require('cors');
+
+let Pots = [
+    {
+        id: 1,
+        name: 'Pot 1',
+        plantId: 1,
+        owner: 1,
+        viewers: [],
+        
+        hardware: {
+            version: 1
+        }
+    }
+]
+let Users = [
+    {
+        id: 1,
+        name: 'Movement',
+    }
+]
+let Plants = [
+    {
+        id: 1,
+        name: 'Carrot',
+        minWater: .2,
+        maxWater: .4,
+        image: '',
+    }
+]
 
 function onStart() {
     console.log("Started Express Server!");
 }
 
+function Authenticate(Request, Response, Next) {
+    Request.user = 1; //TODO: Real logins
+    Next();
+}
+
+function GetPots(Request,Response) {
+    Response.json(Pots.filter(Pot=>Pot.owner==Request.user||Pot.viewers.includes(Request.user)));
+}
+function GetUser(Request,Response) {
+    let User = Users.find(User=>User.id==Request.user);
+    if(!User) {
+        Response.sendStatus(404);
+        return;
+    }
+    Response.json(User);
+}
+function GetPlant(Request,Response) {
+    let Plant = Plants.find(Plant=>Plant.id==Request.params.id);
+    if(!Plant) {
+        Response.sendStatus(404);
+        return;
+    }
+    Response.json(Plant);
+}
+
 
 const ObjExpressServer=new LibExpress();
+ObjExpressServer.use(LibCors({origin:'*'}));
+ObjExpressServer.use('/', Authenticate);
 ObjExpressServer.use('/', LibExpress.static('/App/Client/build'));
-
-ObjExpressServer.get('/api',(Request,Response) => {
-    Response.send("Hello there");
-});
-
+ObjExpressServer.get('/api/pots', GetPots);
+ObjExpressServer.get('/api/user', GetUser);
+ObjExpressServer.get('/api/plant/:id', GetPlant);
 ObjExpressServer.listen(80,onStart);
